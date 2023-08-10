698111d08c8169440a409eaefaca55a0109143fa
Mukesh Kushwaha <73348164+mukesh-kk@users.noreply.github.com>
2023-08-10 17:13:36 +0530

    refactor the code

--- app.js
+++ app.js
@@ -2,23 +2,24 @@
 // dhyan dena ki click hua ki nahi ? --> event Listening 
 // programming language
 
+function doTheMagic(tag,link){
+    document.getElementById(tag).classList.replace('hidden',"show");
+    document.getElementById(tag).setAttribute('src',link);
+
+}
 document
 .getElementById('button1')
 .addEventListener('click',
 ()=>{
 
-document.getElementById('img').classList.replace('hidden',"show");
-document.getElementById('img').setAttribute('src','./assets/happy.png');
-
+    doTheMagic('img','./assets/happy.png');
 }
 )
 document
 .getElementById('button2')
 .addEventListener('click',
 ()=>{
-    
-document.getElementById('img').classList.replace('hidden',"show");
-document.getElementById('img').setAttribute('src','./assets/angry.png');
+    doTheMagic('img','./assets/happy.png');
 
 }
 )
